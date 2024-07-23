b032e2454c2f4fcb1344e22bd1fd25dcab543ef2
Ivanka Tkachenko <64755150+peluhapig@users.noreply.github.com>
2024-07-23 00:09:54 -0400

    Delete ydata_report.py

--- ydata_report.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-df = pd.read_csv('cars_dataset.csv')
-profile = ProfileReport(df, title="Profiling Report")
-profile.to_file("cars_summary_report.html")
